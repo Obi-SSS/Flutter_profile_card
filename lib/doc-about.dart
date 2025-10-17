8996cccdeac61629c9541307d1832ce17ca9b89e
Obi-SSS <ayubicceanputra01@gmail.com>
2025-10-17 09:14:04 +0700

    belajar fundemental debugging

---
+++ lib/doc-about.dart
@@ -0,0 +1,55 @@
+// ignore: dangling_library_doc_comments
+/** penjelelasan */
+
+// ignore: dangling_library_doc_comments
+/**
+ * struktur flutter terdiri dari
+ * 1. .dart tool
+ * 2. .idea
+ * 3. android => untuk knofigurasi android serta 
+ *            => liblary android
+ * 4. build
+ *    => hasil compail nya
+ * 5. ios
+ *    => untuk konfigurasi ios serta
+ *    => liblary ios (native)
+ * 6. lib
+ *    => tempat file dart berada yang nantinya
+ *    => sering di gunakan
+ * 7. linux
+ *    => untuk konfigurasi linux
+ * 8. MacOS
+ *    => untuk konfigurasi macos
+ * 9. test
+ *    => tempat file untuk teasting aplikasi
+ * 10. web
+ *    => untuk konfigurasi web
+ * 11. windows
+ *    => untuk konfigurasi windows
+ * 12. pubsec.yaml
+ *    => tempat konfigurasi aplikasi
+ *    => menambahkan liblary
+ *    => menambahkan asset (gambar, font, dll)
+ * 
+ * di terminal pertintah "fluter pub get"
+ * => untuk meng sincron yang ada di pubspec.yaml
+ * 
+ * kegunaan peruintah perintah "fluter pub update"
+ * =>mengubdate liblary yang ada di pubspec.yaml
+ * 
+ * kegunaan perintah "fluter run"
+ * => menjalankan aplikasi
+ * 
+ * kegunaan perintah "fluter clean"
+ * => membersihkan file bulid
+ * 
+ * liblary dari fluter bisa di lihat dari pub.dev
+ * intl, http, button, dll
+ * 
+ * di dalam dart ada 2 statment
+ * 1. statless widget
+ *    => wiget yang memiliki state (static)
+ * 2. statefull wigate
+ *    => wiget yang memiliki state (dynamic)
+ * 
+ */
