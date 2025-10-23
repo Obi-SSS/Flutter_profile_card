444b2330a4d59a54bdd7b559693fe47cd572a605
Obi-SSS <ayubicceanputra01@gmail.com>
2025-10-23 13:53:29 +0700

    belajar container, padding, dan sizebox

--- lib/profile.dart
+++ lib/profile.dart
@@ -10,32 +10,57 @@ class ProfileCard extends StatelessWidget {
         backgroundColor: Colors.redAccent,
         title: const Text(
           'My profile',
-           style: TextStyle(
-            color: Colors.white,
-            fontWeight: FontWeight.bold,
-           ),
-           ),
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        ),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(
-              'Ayubi Ocean Putra', 
-                style: TextStyle(
-                fontWeight: FontWeight.bold,
+            Container(
+              width: MediaQuery.of(context).size.width * 0.8,
+              height: 100,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: Colors.amber,
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.redAccent,
+                    spreadRadius: 5.0,
+                    blurRadius: 7.0,
+                    offset: Offset(0, 0.3),
+                  ),
+                ],
               ),
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 20,
+                      vertical: 50,
+                    ),
+                    /**
+                     * only hanya memberi 1 posisi
+                     * simetric = memberi sumbu x dan y
+                     * all = memeberi semua jarak
+                     */
+                    child: Text(
+                      'NAME',
+                      style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    'Ayubi Ocean Putra',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ],
               ),
-            Row(
-              children: <Widget>[
-                Column(
-                  children: <Widget>[],
-                ),
-                Column(
-                  children: <Widget>[],
-                ),
-              ],
             ),
+            Column(children: <Widget>[]),
+            Column(children: <Widget>[]),
           ],
         ),
       ),
